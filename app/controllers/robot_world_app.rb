037882b86425fa61cd86af2f3ce473a5a47750d2
Hector Huertas <hectorhuertas@gmail.com>
2015-12-01 19:36:31 -0700

    feat: create and read robots

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,9 +1,30 @@
+require 'models/robot_manager'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/' do
+    erb :main
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    RobotManager.create(params[:robot])
+    redirect '/robots'
+  end
+
+  get '/robots' do
+    @robots = RobotManager.all
     erb :index
   end
+
+  get '/robots/:id' do |id|
+    @robot = RobotManager.find(id.to_i)
+    erb :show
+  end
+
 
   # def call(env)
   #   [200, {'Content-type' => 'text/html'}, ["Hello, 1510!"]]
---
+++ app/models/robot_manager.rb
@@ -0,0 +1,36 @@
+require 'yaml/store'
+require 'models/robot'
+
+class RobotManager
+  def self.database
+    @database ||= YAML::Store.new('db/robot_manager')
+  end
+
+  def self.create(data)
+    database.transaction do
+      database['robots'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['robots'] << { 'id' => database['total'],
+                             'name' => data['name']}
+    end
+  end
+
+  def self.all
+    raw_robots.map { |data| Robot.new(data) }
+  end
+
+  def self.raw_robots
+    database.transaction do
+      database['robots'] || []
+    end
+  end
+
+  def self.raw_robot(id)
+    raw_robots.find { |robot| robot['id']==id}
+  end
+
+  def self.find(id)
+    Robot.new(raw_robot(id))
+  end
+end
